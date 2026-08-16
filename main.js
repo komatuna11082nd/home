d496f85bfb1448680960a789ced3a8443a990cbe
こまつな2nd <komatuna1108.2nd@gmail.com>
2026-08-16 16:15:45 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const title = document.getElementById("title");
+
+//0 ~ maxまでの乱数(整数)を返す
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+rnd = getRandomInt(5)
+console.log(rnd)
+
+if (rnd==0){
+    title.textContent = "こつまな製Webアプリ"
+}
